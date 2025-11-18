331c48b456f3e9f84dc36e127a273baf732aa491
KHKAC <rudgnsrb888@naver.com>
2025-11-18 14:26:24 +0900

    Object Pooling

--- Assets/7_Scripts/MovePipe.cs
+++ Assets/7_Scripts/MovePipe.cs
@@ -7,13 +7,18 @@ public class MovePipe : MonoBehaviour
     [SerializeField] float speed = 0.65f;
     [SerializeField] BoxCollider2D upPipe; // 위 파이프
     [SerializeField] BoxCollider2D downPipe;// 아래 파이프
+    public bool Moving { get; set; } // 오브젝트 풀링을 위해 파이프가 움직일지 말지
 
     void Update()
     {
         // 게임 상태가 PLAY일 때만 움직이도록
         if(GameManager.Instance.GameState == GameManager.State.PLAY)
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            if(Moving) // 값이 true일 때만 움직이게
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            
         }
         else if(GameManager.Instance.GameState == GameManager.State.GAMEOVER)
         {
